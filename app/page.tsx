6a85e2cae25fac702f7846f07d027083113e2fb1
Paulo Filho <paulomacieltorresfilho@gmail.com>
2025-07-15 23:22:31 -0300

    inicia desenvolvimento landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,40 @@
-import { promises as fs } from 'fs';
-import Botao from './ui/Botao';
-
-const TAMANHO_DEFAULT = 20;
-
-export default async function Home() {
-  const text = await fs.readFile('./public/privacypolicy.txt', { encoding: 'utf-8' });
-
+export default function Home() {
   return (
     <div className="container">
-      <div>
-        <h1> Termos e condições: </h1>
-        <div style={{ textAlign: 'justify', border: '1px solid #cecece', overflowWrap: 'break-word', padding: '10px', overflowY: 'scroll', maxHeight: '50vh' }}>
-          {text.split("\n").map((line, index) => <p style={{ fontSize: `${TAMANHO_DEFAULT - (.01 * index)}px` }}>{line}</p>)}
+      <section>
+        <div className="explicacao-grid">
+          <div>
+            <h2>Art 1°</h2>
+            <p>Esta Lei dispõe sobre o tratamento de dados pessoais, inclusive nos <b>meios digitais</b>, por pessoa natural ou por pessoa jurídica de direito público ou privado, com o objetivo de <b>proteger os direitos fundamentais de liberdade e de privacidade e o livre desenvolvimento da personalidade da pessoa</b> natural.</p>
+          </div>
+          <div>
+            <h2>Tópico 2</h2>
+          </div>
+          <div>
+            <h2>Tópico 3</h2>
+          </div>
+          <div>
+            <h2>Tópico 4</h2>
+          </div>
         </div>
-        <Botao />
-      </div>
-    </div >
-  );
+      </section>
+      <section>
+        <h2>Seus Direitos</h2>
+        <div className="direitos-grid">
+          <p className="direito-lgpd destaque alinhadoAEsquerda">acesso aos dados</p>
+          <p className="direito-lgpd" >confirmação da existência de tratamento</p>
+          <p className="direito-lgpd">anonimização, bloqueio ou eliminação de dados desnecessários ou excessivos</p>
+          <p className="direito-lgpd destaque alinhadoADireita" >eliminação dos dados</p>
+          <p className="direito-lgpd destaque alinhadoAEsquerda" >correção dos dados</p>
+          <p className="direito-lgpd">confirmação da existência de tratamento</p>
+          <div className="two-column-grid">
+            <p className="direito-lgpd">informação das entidades públicas e privadas com as quais o Controlador realizou uso compartilhado de dados</p>
+            <p className="direito-lgpd">informação sobre a possibilidade de não fornecer consentimento e sobre consequências da negativa</p>
+          </div>
+          <p className="direito-lgpd destaque centralizado">revogação do consentimento</p>
+        </div>
+      </section>
+
+    </div>
+  )
 }
-
-
