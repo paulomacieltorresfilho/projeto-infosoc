81f16cded24a06a6538ad5934d6f65ffae1c683a
Paulo Filho <paulomacieltorresfilho@gmail.com>
2025-07-16 23:20:03 -0300

    adiciona sessoes e responsividade

--- app/page.tsx
+++ app/page.tsx
@@ -2,60 +2,97 @@ export default function Home() {
   return (
     <div className="container">
       <h1>LGPD: Você conhece seus direitos?</h1>
-      <div className="content">
-        <a href="https://www.planalto.gov.br/ccivil_03/_ato2015-2018/2018/lei/l13709.htm" target="_blank">Link oficial da Lei Geral de Proteção de Dados</a>
-        <section>
-          <div className="explicacao-grid">
-            <div>
-              <h2>O que é a LGPD?</h2>
-              <p style={{ marginBottom: '16px' }}>
-                Esta Lei dispõe sobre o tratamento de dados pessoais, inclusive nos <b>meios digitais</b>, por pessoa natural ou por pessoa jurídica de direito público ou privado, com o objetivo de <b>proteger os direitos fundamentais de liberdade e de privacidade e o livre desenvolvimento da personalidade da pessoa</b> natural.
-              </p>
-              <p>
-                A lei aplica-se a qualquer operação de tratamento realizada por pessoa natural ou por pessoa jurídica de direito público ou privado, <b>independentemente do meio, do país de sua sede ou do país onde estejam localizados os dados</b>.
-              </p>
+      <main>
+        <div className="content">
+          <a href="https://www.planalto.gov.br/ccivil_03/_ato2015-2018/2018/lei/l13709.htm" target="_blank">Link oficial da Lei Geral de Proteção de Dados</a>
+          <section id="explicacao">
+            <div className="explicacao-grid">
+              <div>
+                <h2 id="o-que-e">O que é a LGPD?</h2>
+                <p style={{ marginBottom: '16px' }}>
+                  Esta Lei dispõe sobre o tratamento de dados pessoais, inclusive nos <b>meios digitais</b>, por pessoa natural ou por pessoa jurídica de direito público ou privado, com o objetivo de <b>proteger os direitos fundamentais de liberdade e de privacidade e o livre desenvolvimento da personalidade da pessoa</b> natural.
+                </p>
+                <p>
+                  A lei aplica-se a qualquer operação de tratamento realizada por pessoa natural ou por pessoa jurídica de direito público ou privado, <b>independentemente do meio, do país de sua sede ou do país onde estejam localizados os dados</b>.
+                </p>
+              </div>
+              <div>
+                <h2 id="dados-sensiveis">Dados Sensíveis</h2>
+                <p style={{ marginBottom: '16px' }}>
+                  O dado pessoal é aquele que possibilita a <b>identificação, direta ou indireta</b>, da pessoa.
+                  Dentre eles, há aqueles que exigem maior atenção no tratamento: aqueles relacionados a <b>crianças e adolescentes</b>; e os “sensíveis”, que são os que <b>revelam origem racial ou étnica</b>, convicções <b>religiosas ou filosóficas</b>, opiniões <b>políticas</b>, filiação sindical, questões genéticas, biométricas e sobre a <b>saúde ou a vida sexual</b> de uma pessoa.</p>
+                <p>
+                  O tratamento dos dados <b>sensíveis</b> depende do <b>consentimento explícito do(a) titular dos dados</b> e para um fim definido. E, sem esse consentimento do(a) titular, a LGPD define que <b>somente será possível, quando a informação for indispensável em situações relacionadas a uma obrigação legal</b>.
+                </p>
+              </div>
+              <div>
+                <h2 id="deveres">Deveres dos controladores</h2>
+                <p>Os direitos devem ser garantidos durante toda a existência do tratamento dos dados pessoais realizado pelo órgão ou entidade. Para o exercício dos direitos dos(as) titulares, a LGPD prevê um <b>conjunto de ferramentas que aprofundam obrigações de transparência ativa e passiva</b>, e criam meios processuais para <b>mobilizar a Administração Pública</b>.</p>
+              </div>
+              <div>
+                <h2 id="penalidades">Penalidades</h2>
+                <p style={{ marginBottom: '16px' }}>
+                  As penalidades pelo descumprimento da LGPD são bem pesadas e compreendem: <b>Multas altíssimas</b>, <b>publicização</b> ou até a <b>proibição total ou parcial de atividades</b> que envolvem tratamento de dados.
+                </p>
+                <p>
+                  Além das sanções previstas na LGPD, os funcionários públicos também podem <b>responder por processos administrativos e penais</b> em casos de <b>conduta indevida</b> no tratamento de dados pessoais.
+                </p>
+              </div>
             </div>
-            <div>
-              <h2>Dados Sensíveis</h2>
-              <p style={{ marginBottom: '16px' }}>
-                O dado pessoal é aquele que possibilita a <b>identificação, direta ou indireta</b>, da pessoa.
-                Dentre eles, há aqueles que exigem maior atenção no tratamento: aqueles relacionados a <b>crianças e adolescentes</b>; e os “sensíveis”, que são os que <b>revelam origem racial ou étnica</b>, convicções <b>religiosas ou filosóficas</b>, opiniões <b>políticas</b>, filiação sindical, questões genéticas, biométricas e sobre a <b>saúde ou a vida sexual</b> de uma pessoa.</p>
-              <p>
-                O tratamento dos dados <b>sensíveis</b> depende do <b>consentimento explícito do(a) titular dos dados</b> e para um fim definido. E, sem esse consentimento do(a) titular, a LGPD define que <b>somente será possível, quando a informação for indispensável em situações relacionadas a uma obrigação legal</b>.
-              </p>
+          </section>
+          <section id="seus-direitos">
+            <h2>Seus Direitos</h2>
+            <div className="direitos-grid">
+              <p className="direito-lgpd destaque alinhadoAEsquerda">acesso aos dados</p>
+              <p className="direito-lgpd" >confirmação da existência de tratamento</p>
+              <p className="direito-lgpd">anonimização, bloqueio ou eliminação de dados desnecessários ou excessivos</p>
+              <p className="direito-lgpd destaque alinhadoADireita" >eliminação dos dados</p>
+              <p className="direito-lgpd destaque alinhadoAEsquerda" >correção dos dados</p>
+              <p className="direito-lgpd">confirmação da existência de tratamento</p>
+              <div className="two-column-grid">
+                <p className="direito-lgpd">informação das entidades públicas e privadas com as quais o Controlador realizou uso compartilhado de dados</p>
+                <p className="direito-lgpd">informação sobre a possibilidade de não fornecer consentimento e sobre consequências da negativa</p>
+              </div>
+              <p className="direito-lgpd destaque centralizado">revogação do consentimento</p>
             </div>
-            <div>
-              <h2>Deveres dos controladores</h2>
-              <p>Os direitos devem ser garantidos durante toda a existência do tratamento dos dados pessoais realizado pelo órgão ou entidade. Para o exercício dos direitos dos(as) titulares, a LGPD prevê um <b>conjunto de ferramentas que aprofundam obrigações de transparência ativa e passiva</b>, e criam meios processuais para <b>mobilizar a Administração Pública</b>.</p>
+          </section>
+          <section id="relatos-pessoais">
+            <h2>Relatos Pessoais</h2>
+            <p style={{ marginBottom: "16px" }}>Tem um relato que quer compartilhar conosco? <a href="" target="_blank"><b>Clique aqui!</b></a></p>
+            <div className="relatos-grid">
+              <p className="relato">São muitos pra conseguir relatar todos sucintamente.</p>
+              <p className="relato">Eu tenho experiências junto à Drogasil, junto à Gol, à Dell, Banco do Brasil (inclusive com reclamação no ReclameAqui) e UnB.</p>
+              <p className="relato">Após a retificação de documentos oficiais, e a mudança no sistema do banco, um banco continua recusando a atualizar meu nome do sistema de mensagens e emails, apesar no nome correto estar registrado como titular da conta.</p>
+              <p className="relato">Depois de solicitar o direito de esquecimento das farmácias, eles solicitam 30 dias para a exclusão de dados. E após 2 messes de ter feito este pedido, farmácias ainda mantiveram meus dados, pois ao comprar algo, eles já tinham cadastro com todos meus dados, mesmo sendo que era para ter sido excluído.</p>
+              <p className="relato">Após a minha retificação de nome e gênero, o departamento de matemática não havia alterado meus dados em seus sistemas, mesmo que todos os outros sistemas da UnB haviam alterado estes dados.</p>
+              <p className="relato">Após alterar meu nome no sistema do banco do Nubank, pessoas que já haviam me mandado pix alguma vez ainda recebem meus dados antigos, mesmo tendo ocorrido a mudança de nome no sistema.</p>
+              <p className="relato">O site/app de banco foi alta funçao pra conseguir trocar, nao conseguia a informaçao do que fazer em nenhum lugar. Felizmente, uma atendente disse pra excluir meu cpf e re-inserir, o que finalmente fez funcionar de vez (antes, meu nome morto aparecia as vezes ao abrir o app, e sempre que me enviavam pix). No entanto, no app da 99taxi, nao aceitam minha confirmaçao de identidade de forma alguma, dizendo que o cpf e o nome nao condizem. (Ja sou retificado ha mais de meia década) </p>
             </div>
-            <div>
-              <h2>Tópico 4</h2>
-            </div>
-          </div>
-        </section>
-        <section>
-          <h2>Seus Direitos</h2>
-          <div className="direitos-grid">
-            <p className="direito-lgpd destaque alinhadoAEsquerda">acesso aos dados</p>
-            <p className="direito-lgpd" >confirmação da existência de tratamento</p>
-            <p className="direito-lgpd">anonimização, bloqueio ou eliminação de dados desnecessários ou excessivos</p>
-            <p className="direito-lgpd destaque alinhadoADireita" >eliminação dos dados</p>
-            <p className="direito-lgpd destaque alinhadoAEsquerda" >correção dos dados</p>
-            <p className="direito-lgpd">confirmação da existência de tratamento</p>
-            <div className="two-column-grid">
-              <p className="direito-lgpd">informação das entidades públicas e privadas com as quais o Controlador realizou uso compartilhado de dados</p>
-              <p className="direito-lgpd">informação sobre a possibilidade de não fornecer consentimento e sobre consequências da negativa</p>
-            </div>
-            <p className="direito-lgpd destaque centralizado">revogação do consentimento</p>
-          </div>
-        </section>
-        <section>
-          <h2>Relatos Pessoais</h2>
-        </section>
-        <section>
-          <h2>Mini Glossário</h2>
-        </section>
-      </div>
+
+          </section>
+          <section>
+            <h2>Como ser ouvido e garantir seus direitos?</h2>
+            <p>
+              O <b>canal de interlocução</b> para o tratamento e proteção de dados pessoais no órgão é a Ouvidoria-Geral, por meio da <a href="https://falabr.cgu.gov.br/web/home" target="_blank"><b>Plataforma Fala.BR</b></a>.
+              No Fala.BR é possível registrar <b>manifestações</b> (Ouvidoria) ou <b>pedidos de acesso à informação</b> (SIC).</p>
+          </section>
+          <section id="mini-glossario">
+            <h2>Mini Glossário</h2>
+          </section>
+        </div>
+        <nav>
+          <h2>Índice</h2>
+          <menu>
+            <li><a href="#o-que-e">O que é a LGPD?</a></li>
+            <li><a href="#dados-sensiveis">Dados Sensíveis</a></li>
+            <li><a href="#deveres">Deveres dos controladores</a></li>
+            <li><a href="#penalidades">Penalidades</a></li>
+            <li><a href="#seus-direitos">Seus direitos</a></li>
+            <li><a href="#relatos-pessoais">Relatos pessoais</a></li>
+            <li><a href="#mini-glossario">Mini Glossário</a></li>
+          </menu>
+        </nav>
+      </main>
     </div>
   )
 }
--- app/ui/Botao.tsx
+++
@@ -1,7 +0,0 @@
-"use client";
-
-export default function Botao() {
-    return <button onClick={() => alert("A")}>
-        Continuar
-    </button>
-}
